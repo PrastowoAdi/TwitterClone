4a637b91e83b298f142ba8ae18a6202aacc87129
PrastowoAdi <sinaadi75@gmail.com>
2022-06-06 18:14:49 +0700

    create the widget component search bar

--- components/Feed.js
+++ components/Feed.js
@@ -1,8 +1,26 @@
+/* eslint-disable react/jsx-key */
 import { SparklesIcon } from '@heroicons/react/outline'
 import React from 'react'
 import Input from './Input'
+import Post from './Post'
 
 export default function Feed() {
+    const posts = [
+        {
+            id: '1',
+            name: "Prastowo Adi",
+            username: "prastowoadi",
+            text: 'Nice view',
+            timestamps: '2 hours ago'
+        },
+        {
+            id: '2',
+            name: "Nugroho Adi",
+            username: "nugrohoadi",
+            text: 'Nice brohh',
+            timestamps: '3 hours ago'
+        }
+    ]
   return (
     <div className='xl:ml-[370px] border-l border-r border-gray-200 xl: min-w-[576px] sm:ml-[73px] flex-grow max-w-xl'>
         <div className='flex py-2 px-3 sticky top-0 z-50 bg-white border-b border-gray-200'>
@@ -13,6 +31,12 @@ export default function Feed() {
         </div>
 
         <Input/>
+        {posts.map((post) => (
+            <Post
+                key={post.id} 
+                post={post}
+            />
+        ))}
     </div>
   )
 }
